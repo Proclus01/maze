3f2e6d6a03facb87031121552bce7543684d8bdd
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-11 18:26:05 -0700

    setting up canvas for matter-js demo

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const engine = Engine.create(); // Initialize engine
 const { world } = engine; // Capture world object
 
 const render = Render.create({ // Render.create() takes config object
-    element: document.body, // where to display element
+    // where to display our element (inserts canvas element to HTML)
+    element: document.body, 
     engine: engine,
-    options: { // declare size of canvas
+    options: { // declare size of canvas in pixels
         width: 800,
         height: 600
     }
@@ -19,9 +20,20 @@ Render.run(render); // Activate renderer
 Runner.run(Runner.create(), engine); // Link the runner
 
 // Render a static shape
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
+const shape = Bodies.rectangle(200, 200, 50, 50, { // pos(x), pos(y), len(x), len(y)
+    isStatic: true // show the shape but never move it
 });
 
-// Add the shape object into the world canvas
-World.add(world, shape); 
+// Walls added to borders of canvas
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
+    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
+    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
+    Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
+];
+
+// You can pass in an array of shapes into World.add
+World.add(world, walls); 
+
+// Test rectangle to fall with gravity and hit the walls
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
